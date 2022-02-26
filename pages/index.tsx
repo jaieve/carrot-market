a899313a16ac9431d61067f94120513a6f8ea860
jaieve-ps <yoori0303@naver.com>
2022-02-26 13:32:43 +0900

    [tailwind] test drive

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,32 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-      <div className="bg-red-500">
-        <h2 className="text-black">it works</h2>
+    <div className="bg-slate-400 py-20 px-5 grid gap-10">
+      <div className="bg-white p-10 rounded-2xl shadow-xl">
+        <span className="font-semibold text-3xl">Select Item</span>
+        <div className="flex justify-between my-2">
+          <span className="text-gray-500">Grey Chair</span>
+          <span className="font-semibold">$19</span>
+        </div>
+        <div className="flex justify-between my-2">
+          <span className="text-gray-500">Grey Chair</span>
+          <span className="font-semibold">$19</span>
+        </div>
+        <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
+          <span>Total</span>
+          <span className="font-semibold">$10</span>
+        </div>
+        <div className="mt-5 bg-blue-500 text-white py-3 text-center rounded-xl w-2/4 mx-auto">
+          Checkout
+        </div>
       </div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+    </div>
   );
-}
+  shadow - xl;
+};
 
-export default Home
+export default Home;
