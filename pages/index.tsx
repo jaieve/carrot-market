c26116b5225d2a879d7c97ce3530a88070552336
jaieve-ps <yoori0303@naver.com>
2022-02-26 13:59:51 +0900

    [tailwind] test

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-slate-400 py-20 px-5 grid gap-10">
+    <div className="bg-slate-400 py-20 px-16 grid gap-10">
       <div className="bg-white p-10 rounded-2xl shadow-xl">
         <span className="font-semibold text-3xl">Select Item</span>
         <div className="flex justify-between my-2">
@@ -21,7 +21,28 @@ const Home: NextPage = () => {
           Checkout
         </div>
       </div>
-      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="bg-white overflow-hidden rounded-2xl shadow-xl">
+        <div className="bg-blue-500 p-6 pb-14">
+          <span className="text-white text-2xl">Profile</span>
+        </div>
+        <div className="rounded-xl p-6 bg-white relative -top-5">
+          <div className="flex relative -top-5 itemd-end justify-between">
+            <div className="flex flex-col items-center text-center">
+              <span className="text-sm text-gray-400">Orders</span>
+              <span className="font- ">340</span>
+            </div>
+            <div className="relative -top-12 h-24 w-24 bg-red-400 rounded-full"></div>
+            <div className="flex flex-col items-center text-center">
+              <span className="text-sm text-gray-400">Spent</span>
+              <span className="font-medium">$2,310</span>
+            </div>
+          </div>
+          <div className="relative flex flex-col items-center -mt-10 -mb-5">
+            <span className="text-lg font-semibold">Tony Molloy</span>
+            <span className="text-sm text-gray-500">미국</span>
+          </div>
+        </div>
+      </div>
       <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
       <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
     </div>
