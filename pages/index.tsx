bc75e799c87a213c0b9ca53b3d376d2c8f19634d
jaieve-ps <jaieve1469@polestarhc.com>
2022-02-28 17:22:45 +0900

    [tailwind] modifiers for forms peer

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,22 +3,15 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   // 4.7 강의 5분 46초
   return (
-    <form className="flex flex-col space-y-2  p-5 ">
+    <form className="flex flex-col space-y-2  p-5">
       <input
         type="text"
         required
         placeholder="Username"
-        className="peer rounded-md border border-gray-400 p-1 "
-      />
-      <span className="hidden peer-invalid:block peer-invalid:text-red-500">
-        This input is invalid
-      </span>
-      <span className="hidden peer-valid:block peer-valid:text-teal-500">
-        Awesome username
-      </span>
-      <span className="hidden peer-hover:block peer-hover:text-amber-500">
-        Hello
-      </span>
+        className="required:bg-yellow-500 invalid:bg-gray-300 rounded p-1 peer"      />
+    <span className="hidden peer-invalid:text-red-500 peer-invalid:block">This input is invalid</span>
+    <span className="hidden peer-valid:text-teal-500 peer-valid:block">This input is valid</span>
+    <span className="hidden peer-hover:text-orange-500 peer-hover:block">Hover!</span>
       <input type="submit" value="Login" className="bg-white" />
     </form>
   );
