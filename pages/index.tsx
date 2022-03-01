d8ec4fe437cd17220ce9d5e8e60a845bc58f9240
jaieve-ps <yoori0303@naver.com>
2022-03-01 18:22:15 +0900

    [tailwind] modifier 중첩 가능

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,13 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   // 4.7 강의 5분 46초
   return (
-    <form className="flex flex-col space-y-2  p-5">
+    <div className="flex flex-col space-y-2 p-5">
       <input
-        type="text"
-        required
-        placeholder="Username"
-        className="required:bg-yellow-500 invalid:bg-gray-300 rounded p-1 peer"      />
-    <span className="hidden peer-invalid:text-red-500 peer-invalid:block">This input is invalid</span>
-    <span className="hidden peer-valid:text-teal-500 peer-valid:block">This input is valid</span>
-    <span className="hidden peer-hover:text-orange-500 peer-hover:block">Hover!</span>
-      <input type="submit" value="Login" className="bg-white" />
-    </form>
+        type="file"
+        className="transition-colors file:cursor-pointer file:rounded-xl file:border-0 file:bg-purple-400 file:px-5 file:text-white file:hover:bg-purple-700"
+      />
+      {/* modifier는 중첩이 가능하다! file:hover:options */}
+    </div>
   );
 };
 
