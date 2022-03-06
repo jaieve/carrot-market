6e0db0135d1bc68089253481cc8b609510826548
jaieve-ps <yoori0303@naver.com>
2022-03-06 23:06:17 +0900

    [ui] write

---
+++ pages/community/write.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+
+const Write: NextPage = () => {
+  return (
+    <form className="px-4 py-10">
+      <textarea
+        className="mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-purple-500 focus:ring-purple-500 "
+        rows={4}
+        placeholder="Ask a question!"
+      />
+      <button className="mt-2 w-full rounded-md border border-transparent bg-purple-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-purple-600 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 ">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default Write;
