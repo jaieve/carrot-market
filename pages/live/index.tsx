035b137f527a56255add8bfdaeaeeac323885931
jaieve-ps <yoori0303@naver.com>
2022-03-20 00:42:43 +0900

    5.18 make and apply Component

--- pages/live/index.tsx
+++ pages/live/index.tsx
@@ -1,4 +1,6 @@
 import type { NextPage } from "next";
+import Link from "next/link";
+import FloatingButton from "../../components/floating-button";
 import Layout from "../../components/layout";
 
 const Live: NextPage = () => {
@@ -6,13 +8,16 @@ const Live: NextPage = () => {
     <Layout hasTabBar title="라이브">
       <div className="space-y-4 divide-y-2 py-10">
         {[1, 2, 3, 4, 5].map((_, i) => (
-          <div key={i} className="px-4 pt-4 ">
-            <div className="aspect-video w-full bg-slate-300">
-              <h3 className="mt-2 text-lg text-gray-700">Let's try potatoes</h3>
-            </div>
-          </div>
+          <Link key={i} href={`/live/${i}`}>
+            <a className="block px-4  pt-4">
+              <div className="aspect-video w-full rounded-md bg-slate-300 shadow-sm" />
+              <h1 className="mt-2 text-2xl font-bold text-gray-900">
+                Galaxy S50
+              </h1>
+            </a>
+          </Link>
         ))}
-        <button className="fixed bottom-16 right-5 rounded-full border-transparent bg-purple-400 p-3 text-white shadow-xl transition-colors hover:bg-purple-500">
+        <FloatingButton href="/live/create">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-6 w-6"
@@ -27,7 +32,7 @@ const Live: NextPage = () => {
               d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
             />
           </svg>
-        </button>
+        </FloatingButton>
       </div>
     </Layout>
   );
