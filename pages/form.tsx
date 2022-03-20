ba2fe22c29f7559c27257de27427d4c79ff07f8c
jaieve-ps <yoori0303@naver.com>
2022-03-20 15:11:05 +0900

    7.3 Validation

--- pages/form.tsx
+++ pages/form.tsx
@@ -9,21 +9,35 @@ import { useForm } from "react-hook-form";
 // Easier Inputs ✔
 
 export default function Forms() {
-  const { register, watch } = useForm();
-  console.log(watch());
+  const { register, handleSubmit } = useForm();
+  const onValid = () => {
+    console.log("I'm valid form");
+  };
+  const onInvalid = () => {
+    console.log("I'm INVALID form");
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onValid, onInvalid)}>
       <input
-        {...register("username")} // = name, onChange 이벤트
+        {...register("username", {
+          required: true,
+        })} // = name, onChange 이벤트
         type="text"
-        required
         placeholder="Username"
       />
-      <input {...register("email")} type="email" required placeholder="Email" />
       <input
-        {...register("password")}
+        {...register("email", {
+          required: true,
+        })}
+        type="email"
+        placeholder="Email"
+      />
+      <input
+        {...register("password", {
+          required: true,
+        })}
         type="password"
-        required
         placeholder="Password"
       />
       <input type="submit" value="Create Account" />
