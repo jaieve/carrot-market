fce242ad7b7b7fcc56bdea8f52b38e5c69b816f9
jaieve-ps <yoori0303@naver.com>
2022-03-24 17:44:34 +0900

    9.4 Sending SMS

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,7 +1,10 @@
+import twilio from "twilio";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "@libs/server/client";
 import { prisma } from "@prisma/client";
+
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 async function handler(
   req: NextApiRequest,
@@ -10,7 +13,7 @@ async function handler(
   const { phone, email } = req.body;
   const user = phone ? { phone: +phone } : email ? { email } : null;
   if (!user) return res.status(400).json({ ok: false });
-  const payload = Math.floor(10000 + Math.random() * 90000) + ""; // + "" 는 숫자가 문자열이 된다.
+  const payload = Math.floor(100000 + Math.random() * 90000) + ""; // + "" 는 숫자가 문자열이 된다.
   const token = await client.token.create({
     data: {
       payload,
@@ -30,7 +33,15 @@ async function handler(
       },
     },
   });
-  console.log(token);
+  if (phone) {
+    const message = await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSID,
+      to: process.env.MY_PHONENUMBER!,
+      body: `Your login token is ${payload}`,
+    });
+    console.log(message);
+  }
+
   return res.json({
     ok: true,
   });
