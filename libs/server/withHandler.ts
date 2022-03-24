cc9b01a697d287b24c2ec55958cb0fe2ee0ca863
jaieve-ps <yoori0303@naver.com>
2022-03-24 15:22:53 +0900

    8.4 withHandler

---
+++ libs/server/withHandler.ts
@@ -0,0 +1,24 @@
+// view를 handle하는 역할
+
+import { NextApiRequest, NextApiResponse } from "next";
+
+//function 에 export default를 하지 않으면 누군가 api에 접속했을 때 nextJS에 의해 호출되지 않는다.
+// return함으로써 nextJS에서 실행된다.
+// 어떤 코드를 짜든 return값은 nextJS가 실행할 function이어야 한다.
+export default function withHandler(
+  method: "GET" | "POST" | "DELETE",
+  fn: (req: NextApiRequest, res: NextApiResponse) => void
+) {
+  // this is a funciton which nextJS run
+  return async function (req: NextApiRequest, res: NextApiResponse) {
+    if (req.method !== method) {
+      return res.status(405).end();
+    }
+    try {
+      await fn(req, res);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ error });
+    }
+  };
+}
--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,16 +1,13 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "../../../libs/server/client";
+import withHandler from "../../../libs/server/withHandler";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method !== "POST") {
-    res.status(401).end();
-  }
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.log(req.body);
+  return res.status(200).end();
   // req.body는 req의 인코딩을 기준으로 인코딩된다.
   // resolve : fetch 보낼 때 ehader에 content-type 추가
-  console.log(req.body);
-  // res.json({ ok: true });
 }
 // test url : http://localhost:3000/api/client-test
+
+export default withHandler("POST", handler);
