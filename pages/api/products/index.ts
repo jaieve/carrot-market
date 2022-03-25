c45a8c4c63a446046ec060827dae325ed480c132
jaieve-ps <yoori0303@naver.com>
2022-03-25 14:44:34 +0900

    11.2 Upload API

---
+++ pages/api/products/index.ts
@@ -0,0 +1,38 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    body: { name, price, description },
+    session: { user },
+  } = req;
+  const product = await client.product.create({
+    data: {
+      name,
+      price: +price,
+      description,
+      image: "",
+      user: {
+        connect: {
+          id: user?.id,
+        },
+      },
+    },
+  });
+  res.json({
+    ok: true, // products/upload.tsx 에서 data로 전달되는 값
+    product,
+  });
+}
+
+export default withApiSession(
+  withHandler({
+    method: "POST",
+    handler,
+  })
+);
