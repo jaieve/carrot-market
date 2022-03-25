93bd18f9582795fe92597b524480deb94e5a5ef5
jaieve-ps <yoori0303@naver.com>
2022-03-25 11:51:08 +0900

    10.3 SWR

--- libs/client/useUser.tsx
+++ libs/client/useUser.tsx
@@ -1,7 +1,12 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import useSWR from "swr";
+
+// data를 불러와서 리턴해주는 함수
+const fetcher = (url: string) => fetch(url).then((response) => response.json());
 
 export default function useUser() {
+  const { data, error } = useSWR("/api/users/me", fetcher);
   const [user, setUser] = useState();
   const router = useRouter();
   useEffect(() => {
