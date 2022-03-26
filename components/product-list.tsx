22073b90c15b5a9f913455119f08ee84d9d499aa
jaieve-ps <yoori0303@naver.com>
2022-03-26 16:32:09 +0900

    13.4 Sales, Purchases, Favorites

---
+++ components/product-list.tsx
@@ -0,0 +1,35 @@
+import { ProductWithCount } from "pages";
+import useSWR from "swr";
+import Item from "./item";
+
+interface Record {
+  id: number;
+  product: ProductWithCount;
+}
+
+interface ProductListResponse {
+  [key: string]: Record[];
+}
+
+interface ProductListProps {
+  kind: "Fav" | "Sale" | "Purchase";
+}
+
+export default function ProductList({ kind }: ProductListProps) {
+  const { data } = useSWR<ProductListResponse>(
+    `/api/users/me/record?kind=${kind}`
+  );
+  return data ? (
+    <>
+      {data?.records?.map((record) => (
+        <Item
+          key={record?.id}
+          id={record?.product.id}
+          title={record?.product.name}
+          price={record?.product.price}
+          hearts={record?.product._count?.favs}
+        />
+      ))}
+    </>
+  ) : null;
+}
