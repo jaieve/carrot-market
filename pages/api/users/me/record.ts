dcd9de72a46a4cdec9f0492717158fe037b544f9
jaieve-ps <yoori0303@naver.com>
2022-03-26 15:08:07 +0900

    refactoring for "RECORD & Enum"

---
+++ pages/api/users/me/record.ts
@@ -0,0 +1,35 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    session: { user },
+    query: { kind },
+  } = req;
+  const records = await client.record.findMany({
+    where: {
+      userId: user?.id,
+      kind: kind,
+    },
+    include: {
+      product: true,
+    },
+  });
+  res.json({
+    ok: true,
+    kind,
+    records,
+  });
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ["GET"],
+    handler,
+  })
+);
