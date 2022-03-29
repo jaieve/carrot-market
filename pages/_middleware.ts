d26df40f5d6a91c8099b595f5c61ec3300c8f8ac
jaieve-ps <yoori0303@naver.com>
2022-03-29 15:16:58 +0900

    19.1 Responses and Redirections

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,5 +1,17 @@
 import type { NextRequest, NextFetchEvent } from "next/server";
+import { NextResponse } from "next/server";
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
-  console.log(req.ua); // ua = user agent
+  if (req.ua?.isBot) {
+    return new Response("Plz don't be a bot. Be human", { status: 403 });
+  }
+  console.log(req.url);
+  if (!req.url.includes("/api")) {
+    if (!req.url.includes("/enter") && !req.cookies.eggplantsession) {
+      // url이 enter가 아니고, 세션에 쿠키가 없다면
+      // POST '/api/users/enter ' 의 경우에도 /enter로 redirect 되는 오류는?
+      // /api 를 포함하지 않는 조건으로 감싸주기
+      return NextResponse.redirect(`${req.nextUrl.origin}/enter`);
+    }
+  }
 }
